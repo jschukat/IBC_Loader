2d98f054c3843ca42bc99d519894997cf9b2fe74
Johannes Schukat <j.schukat@celonis.com>
2019-11-20 17:18:48 +0100

    added logging for stderr and stdout

--- cloud_upload.py
+++ cloud_upload.py
@@ -15,6 +15,23 @@ formatter = logging.Formatter(FORMAT)
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
+class LogFile(object):
+    """File-like object to log text using the `logging` module."""
+
+    def __init__(self, name=None):
+        self.logger = logging.getLogger(name)
+
+    def write(self, msg, level=logging.INFO):
+        self.logger.log(level, msg)
+
+    def flush(self):
+        for handler in self.logger.handlers:
+            handler.flush()
+
+# Redirect stdout and stderr
+sys.stdout = LogFile('stdout')
+sys.stderr = LogFile('stderr')
+
 try:
     import requests
     from os import listdir
